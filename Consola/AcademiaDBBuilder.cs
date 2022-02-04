6ea62951716289faf1dbbf04b267600d0c31359d
Lamazu <97883534+Lamazu@users.noreply.github.com>
2022-02-04 06:39:18 -0500

    Add files via upload

---
+++ Consola/AcademiaDBBuilder.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModeloDB;
+using System.Configuration;
+
+namespace Consola
+{
+    public class AcademiaDBBuilder
+    {
+        const string DBTipo = "DBTipo";
+        enum DBTipoConn { SqlServer, Postgres, Memoria }
+        static AcademiaDB db ;
+
+        public static AcademiaDB Crear()
+        {
+            // Lee la configuración acerca de qué base usar del archivo App.config
+            string dbtipo = ConfigurationManager.AppSettings[DBTipo];
+            string conn = ConfigurationManager.ConnectionStrings[dbtipo].ConnectionString;
+            // Construye la conección acorde con el tipo
+            DbContextOptions<AcademiaDB> contextOptions;
+            switch (dbtipo)
+            {
+                case nameof(DBTipoConn.SqlServer) :
+                    contextOptions = new DbContextOptionsBuilder<AcademiaDB>()
+                        .UseSqlServer(conn)
+                        .Options;
+                    break;
+                case nameof(DBTipoConn.Postgres):
+                    contextOptions = new DbContextOptionsBuilder<AcademiaDB>()
+                        .UseNpgsql(conn)
+                        .Options;
+                    break;
+                default: // Por defecto usa la memoria como base de datos
+                    contextOptions = new DbContextOptionsBuilder<AcademiaDB>()
+                        .UseInMemoryDatabase(conn)
+                        .Options;
+                    break;
+            }
+
+            db = new AcademiaDB(contextOptions);
+
+            return db;
+        }        
+    }
+}
